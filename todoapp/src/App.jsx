f505e538e6d187c35276e18a66a94b3813e41aa4
NipuniVithana <ndhananja99@gmail.com>
2024-07-09 10:10:56 +0530

    header added

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,3 +1,6 @@
+import './App.css';
+
+import Header from './components/Header';
 import Todo from './components/Todo';
 
 function App() {
@@ -5,7 +8,9 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <Todo/>
+      
     </div>
   )
 }
